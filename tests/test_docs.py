36accaf25f4e687e0ab8c79fd5c3ebe6a96962e1
tds333 <tds333@mailbox.org>
2026-06-20 18:06:09 +0200

    fix pytest warning, upgrade deps

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -3,7 +3,7 @@ from pytest_examples import CodeExample, EvalExample, find_examples
 
 
 # @pytest.mark.parametrize("example", find_examples("docs", "docs/api.md"), ids=str)
-@pytest.mark.parametrize("example", find_examples("docs/index.md"), ids=str)
+@pytest.mark.parametrize("example", list(find_examples("docs/index.md")), ids=str)
 def test_index_docs(example: CodeExample, eval_example: EvalExample):
     # eval_example.lint(example)
     eval_example.run(example)
